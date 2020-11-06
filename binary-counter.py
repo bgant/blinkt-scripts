0af423d4f10226e421e3c77d0a8a4ed99add86dc
Brandon Gant <bgant@protonmail.com>
2020-11-06 09:26:26 -0600

    should work but not tested

--- binary-counter.py
+++ binary-counter.py
@@ -8,14 +8,17 @@ blinkt.clear()
 blinkt.set_brightness(0.1)
 
 def toggle(LED,STATE):
-    if STATE is 1:
+    if STATE is "1":
         blinkt.set_pixel(LED,0,0,255)
+        print("LED %s is ON" % LED)
     else:
         blinkt.set_pixel(LED,0,0,0)
+        print("LED %s is OFF" % LED)
 
 while True:
     count=(count + 1) % 256  # Modulo counter returns to zero at 256
     BINARY_COUNT='{:08b}'.format(count)
+    print()
     print(BINARY_COUNT)
     for LED in range(8):
         toggle(LED,BINARY_COUNT[LED])
